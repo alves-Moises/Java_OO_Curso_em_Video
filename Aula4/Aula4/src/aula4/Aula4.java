8ca823e32bfc7cc01d7323b3d95dbea864c95724
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-10 23:21:30 -0300

    aula4

---
+++ Aula4/Aula4/src/aula4/Aula4.java
@@ -0,0 +1,15 @@
+package aula4;
+public class Aula4 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("nic", "amarela", 0.4f);
+//        c1.modelo = "abcd";
+//        c1.setPonta(0.5f);
+        c1.status();
+        
+        System.out.println("Tenho uma caneta " + c1.getModelo() + "de ponta " + c1.getPonta());
+    }
+    
+}
---
+++ Aula4/Aula4/src/aula4/Caneta.java
@@ -0,0 +1,48 @@
+package aula4;
+public class Caneta {
+    public String modelo, cor;
+    private float ponta;
+    private boolean tampada;
+
+    public Caneta(String modelo, String cor, float ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+    
+    
+    
+    
+}
